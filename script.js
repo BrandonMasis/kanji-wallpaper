8d52ac73a786234480387b77c1865d306959dcaa
BrandonMasis <brandmasis@gmail.com>
2022-07-02 19:36:24 -0600

    first version

---
+++ script.js
@@ -0,0 +1,30 @@
+let kanji = document.getElementById("kanji");
+let pronun = document.getElementById("kana");
+let div = document.getElementById("photo");
+
+function info() {
+  let userKanji = prompt("kanji");
+  if (userKanji) {
+    kanji.textContent = userKanji;
+  } else {
+    kanji.textContent = "-";
+  }
+  let userPronun = prompt("kana");
+  if (userPronun) {
+    pronun.textContent = userPronun;
+  } else {
+    pronun.textContent = " ";
+  }
+}
+
+function takeshot() {
+  html2canvas(div).then(function (canvas) {
+    document.getElementById("output").appendChild(canvas);
+  });
+}
+
+function reload() {
+  location.reload();
+}
+info();
+takeshot();
